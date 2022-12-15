2895d53c8567a06add82111a11168eb4251ab8ec
ethan-georlette <ethan.georlette1@gmail.com>
2022-12-15 16:35:39 +0200

    worked on home page,added hero

--- front-end/src/cmps/header.jsx
+++ front-end/src/cmps/header.jsx
@@ -1,10 +1,10 @@
 import { NavLink } from "react-router-dom";
-import logo from '../assets/img/logo.png'
+// import logo from '../assets/img/logo.png'
 
 export function AppHeader() {
     return (
         <header className="app-header flex">
-                <img src={logo} alt="" />
+                {/* <img src={logo} alt="" /> */}
                 <NavLink className="fas fa-search" to="/search"></NavLink>
         </header>
     )

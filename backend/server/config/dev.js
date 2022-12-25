ef88f27d97ed482dc3e9164ddcc8086107b07086
ethan-georlette <ethan.georlette1@gmail.com>
2022-12-25 17:06:42 +0200

    started backend

---
+++ backend/server/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // 'dbURL': 'mongodb+srv://Nivivr:DVmywpKh5p0mdYkU@cluster0.mumec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  'dbURL': 'localhost:27020/mydatabase',
+}
+

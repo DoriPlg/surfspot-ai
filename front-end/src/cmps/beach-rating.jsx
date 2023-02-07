e13bcf072c404473e0cb7e3d053db5127c39bdb6
ethan-georlette <ethan.georlette1@gmail.com>
2023-02-07 13:43:39 +0200

    making it prettier with nav bar

---
+++ front-end/src/cmps/beach-rating.jsx
@@ -0,0 +1,10 @@
+import beachImg from '../assets/img/beach.jpg'
+import { Link } from 'react-router-dom';
+
+export function BeachRating({ beach }) {
+    return (
+        <div className="beaches flex center">
+            {/* a preview of a beach and a rating */}
+        </div >
+    )
+}
--- front-end/src/cmps/header.jsx
+++ front-end/src/cmps/header.jsx
@@ -1,11 +1,43 @@
+import React, { Component } from 'react'
 import { NavLink } from "react-router-dom";
 // import logo from '../assets/img/logo.png'
 
-export function AppHeader() {
-    return (
-        <header className="app-header flex">
-                {/* <img src={logo} alt="" /> */}
-                <NavLink className="fas fa-search" to="/search"></NavLink>
-        </header>
-    )
+export class AppHeader extends Component {
+    state = {
+        isNavOpen: false
+    }
+    onNavClick = () => {
+        this.setState(prevState => ({ isNavOpen: !prevState.isNavOpen }))
+        console.log('change')
+    }
+
+    render() {
+        const { isNavOpen } = this.state
+        return (
+            <div className="flex column">
+                <header className="app-header flex">
+                    <div className={isNavOpen ? "fas fa-xmark" : "fas fa-bars"} onClick={this.onNavClick}></div>
+                </header>
+                {isNavOpen &&
+                    <>
+                        <NavLink to="/" className={({ isActive }) => (isActive ? "link active" : "link")}>
+                            Home
+                        </NavLink>
+                        <NavLink to="/bestBeach" className={({ isActive }) => (isActive ? "link active" : "link")}>
+                            Find Beach
+                        </NavLink>
+                        <NavLink to="/test" className={({ isActive }) => (isActive ? "link active" : "link")}>
+                            test enviorment
+                        </NavLink>
+                        <NavLink to="/test" className={({ isActive }) => (isActive ? "link active" : "link")}>
+                            test enviorment
+                        </NavLink>
+                        <NavLink to="/test" className={({ isActive }) => (isActive ? "link active" : "link")}>
+                            test enviorment
+                        </NavLink>
+                    </>
+                }
+            </div>
+        )
+    }
 }

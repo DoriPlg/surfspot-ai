987f0884d5a67cb8818917de14c866c602f368b9
DoriPlg <1dor.peleg@gmail.com>
2023-02-07 21:13:45 +0200

    commit

--- front-end/src/pages/test.jsx
+++ front-end/src/pages/test.jsx
@@ -30,7 +30,8 @@ class _Test extends Component {
         //         beaches = response.data;
         //     });
         const {beaches} =this.state
-        console.log(beachList)
+        console.log(beaches)
+        console.log(typeof(beaches))
         if (!beaches.length) return 'Loading...'
         return (
            <div>
